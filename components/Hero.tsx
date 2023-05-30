1caecb5e9e23ed66b28992873f012fd3258f5e8e
himalMajumder <himalmajumder@gmail.com>
2023-05-30 23:23:39 +0600

    About section done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircle from './BackgroundCircle'
 import Link from 'next/link'
+import MyImage from '../public/himal.jpg'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -19,9 +21,10 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircle />
-      <img className='relative rounded-full h-32 w-32 mx-auto '
+      {/* <img className='relative rounded-full h-32 w-32 mx-auto'
         src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80'
-      />
+      /> */}
+      <Image src={MyImage} className='relative rounded-full h-32 w-32 mx-auto' alt='' />
       <div className='z-20'>
         <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
         <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
